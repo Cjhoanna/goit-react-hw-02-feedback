50905888ccaec9fecc2f1a43aced2ab1f7416ad7
Cjhoanna <127053190+Cjhoanna@users.noreply.github.com>
2023-12-01 21:47:38 -0500

    hola

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+class App extends Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1>hola</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
